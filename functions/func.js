6ea881653374dbe1f653a88d74a739a4c9639d4d
tejaswigowda <tejaswi@foxyninjastudios.com>
2015-09-14 10:12:42 -0700

    ac

--- functions/func.js
+++ functions/func.js
@@ -31,19 +31,49 @@ var sigmaNformula = function(n)
   return n * (n + 1)/2;
 }
 
-var sigmaNrecursive = function(n)
+var sigmaNrecursion = function(n)
 {
   if(n > 1){
-    return n + sigmaNrecursive(n -1);
+    return n + sigmaNrecursion(n -1);
   }
   else{
     return 1
   }
 }
 
+var fibIteration = function(n)
+{
+  var fibArr = [0,1];
+  var i = 3;
+  while(i <= n){
+    fibArr[i-1] = fibArr[i-2] + fibArr[i-3]; 
+    i = i + 1;
+  }
+  //return fibArr[n-1];
+  return fibArr;
+}
+
+var fibRecursion = function(n){
+  if(n == 2){
+    return [0, 1];
+  }
+  else if( n == 1){
+    return [0];
+  }
+  else{
+    var x = fibRecursion(n-1);
+    x[x.length] = x[x.length -1] + x[x.length - 2]
+    return x;
+  }
+}
+
 console.log(addNumbers(45,67));
 console.log(greaterOf(45,67));
 
-console.log(sigmaNformula(45));
-console.log(sigmaNiteration(45));
-console.log(sigmaNrecursion(45));
+console.log(sigmaNformula(10));
+console.log(sigmaNiteration(10));
+console.log(sigmaNrecursion(10));
+
+console.log(fibIteration(10));
+console.log(fibRecursion(10));
+
