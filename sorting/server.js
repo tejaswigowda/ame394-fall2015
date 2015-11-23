5dcb5da27c32ac26a4ced24f5133b082a8e5eb16
tejaswigowda <tejaswi@foxyninjastudios.com>
2015-11-23 09:26:39 -0700

    ac

--- sorting/server.js
+++ sorting/server.js
@@ -1,2 +1,20 @@
-var j = require("./feed.json");
-console.log(j);
+//var j = require("./feed.json");
+// console.log(j.length);
+//
+//
+function selectionSort(arr){
+    return null;
+}
+
+function insertionSort(arr){
+    return null;
+}
+
+function bubbleSort(arr){
+    return null;
+}
+
+var arr = [38, 27, 43,  3,  9, 82, 10]
+console.log(selectionSort(arr));
+console.log(insertionSort(arr));
+console.log(bubbleSort(arr));
