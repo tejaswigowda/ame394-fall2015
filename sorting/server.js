1cf5b3c63e56ee9afb540b93cc3725c05b10a2dc
tejaswigowda <tejaswi@foxyninjastudios.com>
2015-11-25 10:03:10 -0700

    ac

--- sorting/server.js
+++ sorting/server.js
@@ -1,4 +1,8 @@
 var jsonObj = require("./feed.json");
+  for (var i = 0; i < jsonObj.length - 1; i++)
+  {
+    jsonObj[i].date = new Date(jsonObj[i].date).getTime();
+  }
 
 var express = require("express"),
 app = express();
@@ -24,4 +28,26 @@ app.use(errorHandler());
 console.log("Simple static server listening at http://" + hostname + ":" + port);
 app.listen(port);
 
+function selectionSort(key)
+{
+  var arr = jsonObj;
+  for (var i = 0; i < arr.length - 1; i++)
+    {
+        var min = i;  // record the position of the smallest
+        for (var j = i + 1; j < arr.length; j++)
+        {
+        // update min when finding a smaller element
+        if (arr[j][key] < arr[min][key])
+            min = j;
+        }
+ 
+        // put the smallest element at position i
+        var temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+  return arr;
+}
 
+
+console.log(selectionSort("title"));
